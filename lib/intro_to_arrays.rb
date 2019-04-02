3ed133e06e1be585b7392a75d917ce089a970191
virginia koar <virginiakoar@TimKoar.fios-router.home>
2019-04-02 17:54:49 -0400

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,31 +1,32 @@
 def instantiate_new_array
-  my_new_array = []
+  array = []
 end
 
 def array_with_two_elements
-  my_two_array = [1,2]
+array = [1,2]
 end
 
 def first_element(array)
-  array[0]
+array.first
 end
 
 def third_element(array)
-  array[2]
+array[2]
 end
 
 def last_element(array)
-  array[-1]
+array[-1]
 end
 
 def first_element_with_array_methods(array)
-  array.first
+  array.shift
+
 end
 
 def last_element_with_array_methods(array)
-  array.last
+array.pop
 end
 
 def length_of_array(array)
-  array.length
+array.length
 end
